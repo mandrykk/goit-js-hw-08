dbca8aaf246c49d18eae865b876c626707328a41
mandrykk <86601388+mandrykk@users.noreply.github.com>
2021-11-16 15:01:37 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryElem = document.querySelector('.gallery');
+
+galleryElem.addEventListener( 'click', e => {
+    e.preventDefault();
+}, false);
+
+
+function galleryPattern(){
+    const markup = galleryItems
+    .map(
+        ({preview, original, description}) => 
+        `<a class="gallery__item" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join("");
+    galleryElem.insertAdjacentHTML('beforeend', markup);
+
+};
+
+galleryPattern();
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+        captionData: "alt",
+        captionDelay: 250
+    });
+lightbox.on("show.simplelightbox");
 
 console.log(galleryItems);
