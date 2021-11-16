5b71e01c9e0e4862cc434af3d0cf45eeb17b1489
mandrykk <86601388+mandrykk@users.noreply.github.com>
2021-11-16 18:14:15 +0200

    hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,6 @@
 import { galleryItems } from './gallery-items';
 
 import SimpleLightbox from "simplelightbox";
-
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+
+initForm();
+
+feedbackForm.addEventListener('input', e => {
+    e.preventDefault();
+    const formData = new FormData(feedbackForm);
+    formData.forEach((value, name) => console.log(value, name));
+});
+
+feedbackForm.addEventListener('change', throttle(onThottle, 500));
+
+feedbackForm.addEventListener('submit', evt => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+);
+
+function onThottle (e){
+    let persistedFilters = localStorage.getItem(LOCAL_STORAGE_KEY);
+    persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+    persistedFilters[e.target.name] =e.target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(persistedFilters));
+}
+
+function initForm(){
+let persistedFilters = localStorage.getItem(LOCAL_STORAGE_KEY);
+if(persistedFilters){
+    persistedFilters = JSON.parse(persistedFilters);
+    Object.entries(persistedFilters).forEach(([name, value]) => {
+        feedbackForm.elements[name].value = value;
+    });
+}
+}
+
+
 
